dbd3a5811fbc9e00c5960fccf379286a3537c641
oereo <dlstpgns0406@gmail.com>
2020-08-26 18:20:43 +0900

    [ADD] User model

--- Webeing/settings.py
+++ Webeing/settings.py
@@ -132,3 +132,5 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
+AUTH_USER_MODEL = 'Account.User'
