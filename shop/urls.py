3f0321537312bef819de5dd9ad1cff8344ff08fb
oereo <dlstpgns0406@gmail.com>
2020-09-16 13:39:12 +0900

    [ADD] Connect the urls

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'shop'
 
 urlpatterns = [
-    path('', product_in_category, name='product_all'),
-    path('<slug:category_slug>/', product_in_category, name='product_in_category'),
+    path('', restaurant_in_category, name='product_all'),
+    path('<slug:category_slug>/', restaurant_in_category, name='product_in_category'),
     path('<int:id>/<product_slug>/', product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
-def product_in_category(request, category_slug=None):
+def restaurant_in_category(request, category_slug=None):
     current_category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available_display=True)
+    restaurants = Restaurant.objects.filter(available_display=True)
 
 
     if category_slug:
         current_category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=current_category)
+        restaurants = restaurants.filter(category=current_category)
 
     return render(request, 'shop/list.html',
-                  {'current_category': current_category, 'categories': categories, 'products': products})
+                  {'current_category': current_category, 'categories': categories, 'restaurants': restaurants})
 
 from cart.forms import AddProductForm
 def product_detail(request, id, product_slug=None):
