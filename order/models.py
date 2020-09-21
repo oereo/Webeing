6b159a075f3f7a5c15d0638f925764e2812f41a9
oereo <dlstpgns0406@gmail.com>
2020-09-22 02:37:33 +0900

    [FIX] order models.py

--- order/models.py
+++ order/models.py
@@ -7,8 +7,6 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user', null=True, blank=True)
     email = models.EmailField()
     address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
