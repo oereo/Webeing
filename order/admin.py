e0f59d237e88b6ff6f05eec97c134bd5ae05f9a2
oereo <dlstpgns0406@gmail.com>
2020-09-22 03:44:11 +0900

    [FIX] order models.py

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from . import models
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created', 'updated']
+
+
+@admin.register(models.OrderItem)
+class OrderItemAdminn(admin.ModelAdmin):
+    list_display = ['order', 'product', 'price', 'quantity', 'created', 'updated']
+
+   
+
+#admin.site.register(OrderItem)
 
