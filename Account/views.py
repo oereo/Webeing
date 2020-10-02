2942642faa3dc69a1b4b98858d2eede3a5e157dc
oereo <dlstpgns0406@gmail.com>
2020-10-02 16:28:33 +0900

    [ADD] user-profile

--- Account/views.py
+++ Account/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
 from django.contrib import auth
-from .forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
+
+from order.models import OrderItem, Order
+from .forms import UserCreationForm
 from .models import User
 
 
 class UserRegistrationView(CreateView):
-    model = User                            # 자동생성 폼에서 사용할 모델
+    model = User  # 자동생성 폼에서 사용할 모델
     fields = ('email', 'phone_number', 'nickname', 'password')  # 자동생성 폼에서 사용할 필드
 
 
@@ -29,16 +31,16 @@ def signup_customer(request):
         if request.POST['password'] == request.POST['password_confirm']:
             user = User.objects.create_user(
                 # request.POST['email'],
-                phone_number = request.POST['phonenumber'],
-                password = request.POST['password'],
-                email = request.POST['username'],
-                nickname = request.POST['nickname'],
-                date_of_birth = request.POST['dateofbirth'],
+                phone_number=request.POST['phonenumber'],
+                password=request.POST['password'],
+                email=request.POST['username'],
+                nickname=request.POST['nickname'],
+                date_of_birth=request.POST['dateofbirth'],
             )
             auth.login(request, user)
 
         return redirect('/main/')
-        
+
     else:
         form = UserCreationForm()
     return render(request, 'signupcustomer.html', {'form': form})
@@ -49,19 +51,22 @@ def signup_seller(request):
         if request.POST['password'] == request.POST['password_confirm']:
             user = User.objects.create_seller(
                 # request.POST['email'],
-                phone_number = request.POST['phonenumber'],
-                password = request.POST['password'],
-                email = request.POST['username'],
-                nickname = request.POST['nickname'],
-                date_of_birth = request.POST['dateofbirth'],
-                seller_address = request.POST['seller_address'],
-                business_number = request.POST['business_number'],
-                seller_name = request.POST['seller_name'],
+                phone_number=request.POST['phonenumber'],
+                password=request.POST['password'],
+                email=request.POST['username'],
+                nickname=request.POST['nickname'],
+                date_of_birth=request.POST['dateofbirth'],
+                seller_address=request.POST['seller_address'],
+                business_number=request.POST['business_number'],
+                seller_name=request.POST['seller_name'],
             )
             auth.login(request, user)
 
+
         return redirect('/main/')
-        
+
+
+
     else:
         form = UserCreationForm()
     return render(request, 'signupseller.html', {'form': form})
@@ -76,12 +81,20 @@ def logout(request):
 
 
 def customer_page(request):
-    return render(request, 'customerPage.html')
+    user = request.user
+    orders = Order.objects.filter(user=user)
+    order_items_list = []
 
+    for order in orders:
+        order_items_list.append(OrderItem.objects.filter(order=order))
+    # orderitems = OrderItem.objects.filter(order=order[0])
+    # orderitems = get_object_or_404(Order, pk=order_id)
+    # orderitems = Order.objects.values('user', 'OrderItem__created')
+    # orderitems = Order.objects.filter(user=user).select_related('user')
+    return render(request, 'customerPage.html', {'order_items_list': order_items_list, 'orders': orders})
+    # return render(request, 'customerPage.html')
 
 def seller_page(request):
-    user = request.user
-
     return render(request, 'sellerPage.html')
 
 
@@ -95,4 +108,3 @@ def tos(request):
 
 def tos_user(request):
     return render(request, 'TOS_user.html')
-
