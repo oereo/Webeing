4102bb153299e790b247d23f250f5f486bbd3503
oereo <dlstpgns0406@gmail.com>
2020-10-08 14:08:08 +0900

    [ADD] card.css about card-design

--- shop/urls.py
+++ shop/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     path('<slug:category_slug>/', restaurant_in_category, name='restaurant_in_category'),
     path('main/<slug:restaurant_slug>/', product_in_restaurant, name='product_in_restaurant'),
     path('<int:id>/<product_slug>/', product_detail, name='product_detail'),
-
-<<<<<<< HEAD
     path('sellerPage/register/',ProductRegister.as_view(),name='product_register'), 
-=======
->>>>>>> 2bad7f8a14aaf9731b16dedc88398c1dceeab3d2
+
 ]
