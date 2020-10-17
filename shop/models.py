3711a8d796a623eb87e206041e83e42f98367b4d
oereo <dlstpgns0406@gmail.com>
2020-10-17 17:38:01 +0900

    [ADD] discount

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from Account.models import User
 from django.urls import reverse
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     meta_description = models.TextField(blank=True)
@@ -24,7 +25,7 @@ class Restaurant(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='restaurants')
     name = models.CharField(max_length=200, db_index=True)
 
-    image = models.ImageField(upload_to='restaurants/%Y/%m/%d',blank=True)
+    image = models.ImageField(upload_to='restaurants/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     meta_description = models.TextField(blank=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
@@ -32,7 +33,7 @@ class Restaurant(models.Model):
 
     available_display = models.BooleanField('Display', default=True)
     available_order = models.BooleanField('Order', default=True)
-    
+
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -42,7 +43,7 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('shop:product_in_restaurant', args=[self.slug])
 
@@ -52,11 +53,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
 
-    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     meta_description = models.TextField(blank=True)
-
-    price = models.IntegerField() ## [찬규] 가격 정보 정수값으로 수정했습니다.
+    origin_price = models.IntegerField()
+    price = models.IntegerField()  ## [찬규] 가격 정보 정수값으로 수정했습니다.
+    discount = models.IntegerField(default=0)
     stock = models.PositiveIntegerField()
 
     available_display = models.BooleanField('Display', default=True)
@@ -69,7 +71,7 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['-created']
-        index_together = [['id','slug']]
+        index_together = [['id', 'slug']]
 
     def __str__(self):
         return self.name
@@ -81,9 +83,9 @@ class Product(models.Model):
 class buyingList(models.Model):
     buylist = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='buylists')
     name = models.CharField(max_length=200, db_index=True)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    
+
     def __str__(self):
         return self.name
 
@@ -91,12 +93,11 @@ class buyingList(models.Model):
 class Register(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name='Registers')
     name = models.CharField(max_length=200, db_index=True)
-    
-    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
-    description = models.TextField(blank=True)
-   
 
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
 
     available_display = models.BooleanField('Display', default=True)
@@ -112,5 +113,3 @@ class Register(models.Model):
 
     def __str__(self):
         return self.name
-
-
