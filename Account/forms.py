6e8cccec401631f8af7165670a248b9bfe518529
seungjohan <cmostrillion@gmail.com>
2020-10-17 15:53:32 +0900

    [ADD] first commit of user-envi

--- Account/forms.py
+++ Account/forms.py
@@ -15,7 +15,7 @@ class UserCreationForm(forms.ModelForm):
     phone_number = forms.CharField(label = 'phone_number')
     class Meta:
         model = User
-        fields = ('email', 'nickname', 'phone_number','date_of_birth', 'password')
+        fields = ('email', 'nickname', 'phone_number', 'password')
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -37,7 +37,7 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'date_of_birth','nickname','phone_number',
+        fields = ('email', 'password', 'nickname','phone_number',
                   'is_active', 'is_admin')
 
     def clean_password(self):
